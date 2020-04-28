3b10af50def3910b391a650b796109891c5197d5
RAHUL SARASWAT <rahul.saraswat@inspopindia.com>
2020-04-28 18:30:49 +0530

    Maximum Subarray Solution

---
+++ FindMaximumSubarray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    /*  Problem 3 : Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum. 
+    *  Example Array - Input: [-2,1,-3,4,-1,2,1,-5,4] 
+    *  Output : 6
+    *  Explanation: [4,-1,2,1] has the largest sum = 6.
+    */
+    public class FindMaximumSubarray
+    {
+        public static Tuple<List<int>, int> GetMaximumSubArray(int[] nums)
+        {
+            int maximum_end_here = 0; int maximum_so_far = 0; int start = 0; int end = 0; int temp = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                maximum_end_here = maximum_end_here + nums[i];
+                if (maximum_end_here < 0)
+                {
+                    maximum_end_here = 0;
+                    temp = i + 1;
+                }
+                if(maximum_so_far < maximum_end_here)
+                {
+                    maximum_so_far = maximum_end_here;
+                    start = temp;
+                    end = i;
+                }                    
+            }
+            List<int> resultArray= new List<int>();
+            for (int j = start; j <= end; j++)
+                resultArray.Add(nums[j]);
+            return Tuple.Create(resultArray, maximum_so_far);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,21 +7,28 @@ namespace LeetCode.Problems
         static void Main(string[] args)
         {
             //Find single non-repetitive number from an array using Bit manipulation approach (XOR bitwise operator)
-            int[] intArray = { 4, 1, 2, 1, 2 };
+            //int[] intArray = { 4, 1, 2, 1, 2 };
             //Console.WriteLine($"Unique Number is {FindSingleNonRepetitiveNumberFromArray.GetSingleNumber(intArray)} from [4,1,2,1,2] array.");
 
-            int[] intArray1 = { 5, 4, 2, 4, 5, 6, 6, 2, 7 };
+            //int[] intArray1 = { 5, 4, 2, 4, 5, 6, 6, 2, 7 };
             //Console.WriteLine($"Unique Number is {FindSingleNonRepetitiveNumberFromArray.GetSingleNumber(intArray1)} from [5, 4, 2, 4, 5, 6, 6, 2, 7] array.");
 
-            Console.Write("Please enter a number : ");
-            int input1 = Convert.ToInt32(Console.ReadLine());
-            string result = FindHappyNumber.CheckIfInputNumberIsHappy(input1) ? "a Happy Number." : "not a Happy Number.";
-            Console.WriteLine($"Input number {input1} is {result}");
-            Console.Write("Please enter another number : ");
-            int input2 = Convert.ToInt32(Console.ReadLine());
-            string result2 = FindHappyNumber.CheckIfInputNumberIsHappy(input2) ? "a Happy Number." : "not a Happy Number.";
-            Console.WriteLine($"Input number {input2} is {result2}");
+            //Console.Write("Please enter a number : ");
+            //int input1 = Convert.ToInt32(Console.ReadLine());
+            //string result = FindHappyNumber.CheckIfInputNumberIsHappy(input1) ? "a Happy Number." : "not a Happy Number.";
+            //Console.WriteLine($"Input number {input1} is {result}");
+            //Console.Write("Please enter another number : ");
+            //int input2 = Convert.ToInt32(Console.ReadLine());
+            //string result2 = FindHappyNumber.CheckIfInputNumberIsHappy(input2) ? "a Happy Number." : "not a Happy Number.";
+            //Console.WriteLine($"Input number {input2} is {result2}");
 
+            //Problem 3
+            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            var result = FindMaximumSubarray.GetMaximumSubArray(nums);
+            Console.WriteLine($"Maximun SubArray is [{string.Join(",", result.Item1.ToArray())}] from [{string.Join(",", nums)}] array.");
+            Console.WriteLine($"Sum of Maximun SubArray is [{result.Item2}] from [{string.Join(",", nums)}] array.");
+            //Console.WriteLine(string.Join(",", result.Item1.ToArray()));
+            //Console.WriteLine(result.Item2);
             Console.ReadLine();
         }
     }
